37d34dd3e826023d591c46d3465d000bd70f1903
tcbtck <forbiddensite403@hotmail.com>
2013-07-22 16:24:36 +0800

    erg

--- FileHaven_2.4/src/database/DB.java
+++ FileHaven_2.4/src/database/DB.java
@@ -9,14 +9,14 @@ public class DB {
     
     public static String dbdriver = "com.mysql.jdbc.Driver";
     
-//    public static String url = "jdbc:mysql://localhost/FileHaven";
-//    public static String username = "root";
-//    public static String password = "root";
+    public static String url = "jdbc:mysql://localhost/FileHaven";
+    public static String username = "root";
+    public static String password = "root";
     
   
-    public static String url = "jdbc:mysql://adeeldb.ch30tsalfl52.ap-southeast-1.rds.amazonaws.com:3306/filehaven";
-    public static String username = "adeeldb";
-    public static String password = "Iamtheking";
+//    public static String url = "jdbc:mysql://adeeldb.ch30tsalfl52.ap-southeast-1.rds.amazonaws.com:3306/filehaven";
+//    public static String username = "adeeldb";
+//    public static String password = "Iamtheking";
     
     
     public DB() throws Exception {
